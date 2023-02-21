e87bd0923b604742465d557630fe1a6af21e579b
lszh02 <464575284@qq.com>
2023-02-21 17:44:22 +0800

    delete.py增加删除DDD。

--- core/delete_record/delete.py
+++ core/delete_record/delete.py
@@ -30,9 +30,13 @@ def delete_record(del_num, web_driver):
         if i > 0 and i % 100 == 0:
             # 每删除100条记录需点击“下一页”
             wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, 'div.pagination a[title="下一页"]'))).click()
-        # 点击删除
+        # 点击删除处方
+        # wait.until(
+        #     ec.element_to_be_clickable((By.CSS_SELECTOR, '#outpatientTable tr:nth-child(2) a[title="删除"]'))).click()
+
+        # 点击删除DDD
         wait.until(
-            ec.element_to_be_clickable((By.CSS_SELECTOR, '#outpatientTable tr:nth-child(2) a[title="删除"]'))).click()
+            ec.element_to_be_clickable((By.CSS_SELECTOR, '#prevMedicalTable tr:nth-child(2) a[title="删除"]'))).click()
 
         # 点击确定删除
         wait.until(ec.alert_is_present())
@@ -50,9 +54,17 @@ if __name__ == '__main__':
     wait_time = 60  # 等待网页相应时间
     web_driver.implicitly_wait(wait_time)  # 隐式等待
     wait = WebDriverWait(web_driver, wait_time, poll_frequency=0.2)  # 显式等待
-
+    # 登录
     login(web_driver)
-    del_num = input('需要删除多少条记录？————>')
-    delete_record(int(del_num), web_driver)
-    input('完成删除！确认输入Yes')
 
+    input_text = input('需要删除多少条记录？————>')
+    delete_record(int(input_text), web_driver)
+
+    input_text = input('如需继续删除，请输入数字!  退出请按回车————>')
+    while True:
+        if input_text == '':
+            print('bye')
+            break
+        else:
+            delete_record(int(input_text), web_driver)
+            input_text = input('如需继续删除，请输入数字!  退出请按回车————>')
