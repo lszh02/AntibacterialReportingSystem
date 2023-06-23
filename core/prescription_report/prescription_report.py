444a608fe7047db4fb107ca8baf1adbd118c4f84
lszh03 <464575284@qq.com>
2023-06-23 11:15:54 +0800

    优化“用药频次”和“给药方式”字典。无匹配条目时使用缺省值。

--- core/prescription_report/prescription_report.py
+++ core/prescription_report/prescription_report.py
@@ -226,7 +226,8 @@ class PrescriptionReport:
             if drug_name in self.ddd_drug_dict:
                 self.web_driver.find_element(By.CSS_SELECTOR,
                                              '#outpatientTable tr:nth-child(2) div:nth-child(2)').click()  # 单击“有”
-                self.web_driver.find_element(By.CSS_SELECTOR, '#outpatientTable input[value="录入详细信息"]').click()  # 单击录入
+                self.web_driver.find_element(By.CSS_SELECTOR,
+                                             '#outpatientTable input[value="录入详细信息"]').click()  # 单击录入
                 self.input_antibacterial(drug_list)
                 break
         else:
@@ -282,16 +283,36 @@ class PrescriptionReport:
 
     def input_other_details(self, one_drug_info, one_row_unit):
         freq_web_list = ['即刻', '1/日', '2/日', '3/日', '4/日', 'q2h', 'q6h', 'q8h', 'q12h', '每晚', '其他']
-        way_web_list = ['静脉滴注', '静脉泵入', '静脉推注', '肌肉注射', '静脉注射', '皮下注射', '球后注射', '结膜下注射', '眼内注射', '直肠给药', '雾化吸入', '肠道准备',
+        way_web_list = ['静脉滴注', '静脉泵入', '静脉推注', '肌肉注射', '静脉注射', '皮下注射', '球后注射',
+                        '结膜下注射', '眼内注射', '直肠给药', '雾化吸入', '肠道准备',
                         '口服', '外用', '滴鼻', '滴耳', '滴眼', '鞘内注射', '腹膜透析', '皮试']
         dose_unit_web_list = ['克', '毫克', '万单位', '滴', 'ml', '片', '支', '粒', '瓶', '包', '袋']
-        freq_dict = {'QN': '每晚', 'QD': '1/日', 'Q12H': 'q2h', 'P.R.N': '其他', 'ONCE': '即刻', 'QID': '4/日',
-                     'BIW1': '其他', 'BID': '2/日',
-                     'TID': '3/日', '(空白)': '其他'}
-        way_dict = {'口服': '口服', '血液透析 ': '腹膜透析', '外用': '外用', '肌肉注射': '肌肉注射', '吸入': '雾化吸入', '涂眼睑内': '外用', '滴眼': '滴眼',
-                    '(空白)': '皮试', '皮下注射(不带费用、耗材）': '皮下注射', '静脉注射(麻醉科专用)': '静脉注射', '塞肛': '外用', '含服': '口服', '局麻用': '肌肉注射',
+        freq_dict = {'ONCE': '即刻',
+                     'QD': '1/日',
+                     'BID': '2/日',
+                     'TID': '3/日',
+                     'QID': '4/日',
+                     'Q2H': 'q2h',
+                     'Q6H': 'q6h',
+                     'Q8H': 'q8h',
+                     'Q12H': 'q2h',
+                     'QN': '每晚',
+                     '(空白)': '其他'}
+        way_dict = {'口服': '口服',
+                    '血液透析 ': '腹膜透析',
+                    '外用': '外用',
+                    '肌肉注射': '肌肉注射',
+                    '吸入': '雾化吸入',
+                    '涂眼睑内': '外用',
+                    '滴眼': '滴眼',
+                    '皮下注射(不带费用、耗材）': '皮下注射',
+                    '静脉注射(麻醉科专用)': '静脉注射',
+                    '塞肛': '外用',
+                    '含服': '口服',
+                    '局麻用': '肌肉注射',
                     '喷喉': '外用'}
-        dose_unit_dict = {'丸': '粒', 'ug': '粒', '吸': '粒', 'ml': 'ml', '片': '片', 'g': '克', '粒': '粒', 'mg': '毫克'}
+        dose_unit_dict = {'丸': '粒', 'ug': '粒', '吸': '粒', 'ml': 'ml', '片': '片', 'g': '克', '粒': '粒',
+                          'mg': '毫克'}
 
         try:
             # 输入抗菌药金额
@@ -308,11 +329,11 @@ class PrescriptionReport:
 
             # 选择用法（频次）
             Select(self.web_driver.find_element(By.ID, "medicineFrequency")).select_by_visible_text(
-                freq_dict.get(one_drug_info.get('frequency')))
+                freq_dict.get(one_drug_info.get('frequency'), '其他'))
 
             # 选择给药途径
             Select(self.web_driver.find_element(By.ID, "medicineWay")).select_by_visible_text(
-                way_dict.get(one_drug_info.get('usage')))
+                way_dict.get(one_drug_info.get('usage'), '口服'))
         except Exception as e:
             print(f'输入抗菌药时出错，报错信息：{e}')
         print(f'请确认抗菌药输入无误后单击右键继续……')
